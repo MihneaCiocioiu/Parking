46d57c0ef2611d52fc74bdb5dc00787ce0ee7388
MihneaCiocioiu <mihnea.iulian03@gmail.com>
2021-05-25 01:54:18 +0300

    Updated the visualisation

--- Parking.cpp
+++ Parking.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <Windows.h>
+#include <cstdlib>
 #include <stdlib.h>
 #include <iostream>
 #include "parkingLot.h"
@@ -9,19 +10,35 @@
 
 using namespace std;
 parkingLot a,b,c;
+parkingLot lot[4][4];
 
 int main() {
-    a=parkingLot(30);
-    b=parkingLot(50);
-    c=parkingLot(45);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i == 0) lot[i][j] = parkingLot(30, "orange");
+            if (i == 1) lot[i][j] = parkingLot(30, "blue");
+            if (i == 2) lot[i][j] = parkingLot(30, "purple");
+        }
+    }
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole,  FOREGROUND_RED|FOREGROUND_INTENSITY);
+    /*  SetConsoleTextAttribute(hConsole,  FOREGROUND_RED|FOREGROUND_INTENSITY);
     cout << a.getSize() << "\n";
     a.show({ 0,0 });
     int width = a.getWidth();
     b.show({ 30,0 });
     SetConsoleTextAttribute(hConsole,  FOREGROUND_BLUE|FOREGROUND_INTENSITY);
-    c.show({ 0,15 });
+    c.show({ 0,18 }); */
+    int row = rand() % 3;
+    int col = rand() % 3;
+    int place = rand() % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (i==0) SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
+            else if (i==1) SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+            else SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN|FOREGROUND_INTENSITY);
+            lot[i][j].show({(short)j* (short) (max(13,lot[i][j].getWidth())),(short)i * 16 });
+        }
+    }
   //  SetConsoleCursorPosition(hConsole, { 8,15 });
    // a.show({ 8,15 });
 }
--- parkingLot.h
+++ parkingLot.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iostream>
 #include "car.h"
+#include <cstring>
 
 using namespace std;
 
@@ -10,11 +11,23 @@ class parkingLot {
 private:
 	int capacity;
 	int length=6;
+	int id;
+	const char* type;
 	vector<vector<car>> a;
 public:
 	parkingLot() {
 		capacity = 0;
 		a.clear();
+	}
+	parkingLot(int size, const char *Type) {
+		capacity = size;
+		type = Type;
+		a.resize(length, vector<car>(capacity / length, 0));
+		if (capacity % length != 0) {
+			for (int i = 0; i < capacity % length; i++) {
+				a[i].push_back(car(0));
+			}
+		}
 	}
 	parkingLot(int size) {
 		capacity = size;
@@ -41,11 +54,20 @@ public:
 		return capacity;
 	}
 	int getWidth() {
-		if (capacity % length) return 1 + capacity / length * 3 + 3;
-		else return  1 + capacity / length * 3;
+		if (capacity % length) return 2 + capacity / length * 4 + 4;
+		else return  2 + capacity / length * 4;
 	}
 	void show(_COORD coord) {
 		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		SetConsoleCursorPosition(hConsole, coord);
+		cout << "Id: " << capacity;
+		coord.Y++;
+		SetConsoleCursorPosition(hConsole, coord);
+		cout << "Type: " << type;
+		coord.Y++;
+		SetConsoleCursorPosition(hConsole, coord);
+		cout << "Capacity: " << capacity;
+		coord.Y++;
 		SetConsoleCursorPosition(hConsole, coord);
 		cout << (char)218;
 		int L = capacity / length;
@@ -61,16 +83,18 @@ public:
 			for (int j = 0; j < L; j++) {
 				cout << (char)179;
 				int id = a[i][j].getID();
-				if (id <= 9) cout << "  ";
+				if (id == 0) cout << "   ";
+				else if (id <= 9) cout << "  ";
 				else if (id <= 99) cout << " ";
-				cout << id;
+				else cout << id;
 			}
 			if (i < l && l != 0) {
 				cout << (char)179;
 				int id = a[i][L].getID();
-				if (id <= 9) cout << "  ";
+				if (id==0) cout<<"   ";
+				else if (id <= 9) cout << "  ";
 				else if (id <= 99) cout << " ";
-				cout << id;
+				else cout << id;
 			}
 			cout << (char)179;
 			coord.Y++;
@@ -83,7 +107,7 @@ public:
 				if (l != 0 && i < l) {
 					cout << (char)196 << (char)196 << (char)196 << (char)197;
 				}
-				cout << (char)196 << (char)196 << (char)196;// << (char)180;
+				cout << (char)196 << (char)196 << (char)196;
 				if (i == l - 1 && l) cout << (char)217;
 				else cout << (char)180;
 				coord.Y++;
